4a639ed6b505290ad38062b7105cd36934ef238d
Nisseliu <nilba239@student.liu.se>
2020-11-16 20:56:45 +0100

    First commit

---
+++ cmake-build-debug/CMakeFiles/lab1upg1.dir/depend.make
@@ -0,0 +1,2 @@
+# Empty dependencies file for lab1upg1.
+# This may be replaced when dependencies are built.
---
+++ main.cpp
@@ -0,0 +1,39 @@
+#include <iostream>
+#include <cmath>
+#include <iomanip>
+
+using namespace std;
+
+int main() {
+
+    cout << "Ange startvärde: ";
+    double start;
+    cin >> start;
+
+    cout << endl;
+
+
+    cout << "Ange slutvärde: " << endl;
+    double slut;
+    cin >> slut;
+
+    while (slut < 0) {
+        cout << "Fel" << endl;
+        cout << "Ange slutvärde: " << endl;
+        cin >> slut;
+    }
+
+    cout << "Celsius" << setw(15) << "Kelvin" << setw(20) << "Farenheit" << setw(17) << "Reaumur" << endl;
+    cout << "................................................................" << endl;
+    for (int i =0; i < slut; ++i){
+        cout << setw(2) << start +  i;
+        cout << setw(20) << setprecision(5) << start + i + 273.15;
+        cout << setw(17) << setprecision(3) << start + i * 1.8 + 32.00 << setw(18) << start + i * 0.8 << endl;
+
+
+    }
+
+
+
+    return 0;
+}
